4b94dd87d4ea8fcc5806b2ea1a275138c6471893
Rodolphe <dutchr@live.fr>
2023-11-24 16:32:08 +0100

    :tada: adding create in controller & route

---
+++ userModel.js
@@ -0,0 +1,9 @@
+const { mongoose, Schema } = require("mongoose");
+
+const userSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
